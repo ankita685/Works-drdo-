fa468913cc1869caa03fba71d9354649eeed3aac
Ankita choudhary <99496688+ankita685@users.noreply.github.com>
2024-05-29 13:20:07 +0530

    Update firebase-config.jsx

--- src/firebase-config.jsx
+++ src/firebase-config.jsx
@@ -1,31 +1,17 @@
-// Import the functions you need from the SDKs you need
-// Import the functions you need from the SDKs you need
+
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { useState, useEffect } from 'react';
 
 import 'firebase/database';
-// import { getDatabase, ref, onValue } from 'firebase/database';
+
 import { getDatabase, ref, onValue } from "firebase/database";
 
 
 
-// Reference to the desired data in the Firebase Realtime Database
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCLycLa9YcfXDB6EYleynEfxiowVGfb0oA",
-  authDomain: "vite-project-4dcf8.firebaseapp.com",
-  databaseURL: "https://vite-project-4dcf8-default-rtdb.firebaseio.com",
-  projectId: "vite-project-4dcf8",
-  storageBucket: "vite-project-4dcf8.appspot.com",
-  messagingSenderId: "76185407780",
-  appId: "1:76185407780:web:a0387c4ea6fcbc09dea3b1",
-  measurementId: "G-V6NFWZ2WCL"
+  //paste your config code here...
 };
 
 
@@ -33,8 +19,6 @@ const firebaseConfig = {
 const firebaseApp = initializeApp(firebaseConfig);
 
 const database = getDatabase(firebaseApp);
-// firebase.database().goOffline();
-// database.goOffline();
-// database.goOffline();
+
 
 export { firebaseApp, database };
